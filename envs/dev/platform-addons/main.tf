b52597143ae673c711728bd5e6b7cae2e880b9d3
Shinsolmi <Shinsolmi>
2026-06-11 16:16:20 +0900

    secure: Grafana 패스워드를 Secrets Manager 참조 구조로 전환하여 평문 노출 차단

--- envs/dev/platform-addons/main.tf
+++ envs/dev/platform-addons/main.tf
@@ -1,5 +1,21 @@
 # envs/dev/platform-addons/main.tf
 
+# ==============================================================================
+# 🔐 AWS Secrets Manager에서 실시간으로 비밀번호 금고 낚아채기
+# ==============================================================================
+data "aws_secretsmanager_secret" "grafana_secret" {
+  name = "team1-dev-grafana-credentials" # 1층 infra 레이어에서 생성했던 금고 명칭
+}
+
+data "aws_secretsmanager_secret_version" "grafana_secret_val" {
+  secret_id = data.aws_secretsmanager_secret.grafana_secret.id
+}
+
+locals {
+  # 금고 안에 {"password": "비밀번호원문"} 구조로 보관된 값을 추출하여 로컬 변수에 안착
+  grafana_password = jsondecode(data.aws_secretsmanager_secret_version.grafana_secret_val.secret_string)["password"]
+}
+
 # ------------------------------------------------------------------------------
 # 🚀 1. ArgoCD GitOps 엔진 주입 
 # ------------------------------------------------------------------------------
@@ -40,7 +56,7 @@ resource "helm_release" "prometheus_stack" {
     yamlencode({
       grafana = {
         enabled       = true
-        adminPassword = data.terraform_remote_state.infra.outputs.grafana_admin_password
+        adminPassword = local.grafana_password
       }
     })
   ]
