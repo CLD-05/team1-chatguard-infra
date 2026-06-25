c58accbc883f5783c378dbff6118c84a1bdbcce8
Kimwooyeon0032 <dndus385@naver.com>
2026-06-25 23:27:53 +0900

    team 태그 추가

--- envs/dev/infra/iam.tf
+++ envs/dev/infra/iam.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "chat_server_s3_trust" {
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:chatguard:team1-dev-chat-server-sa"]
+      values   = ["system:serviceaccount:chatguard:team1-dev-api-server-sa"]
     }
   }
 }
@@ -28,9 +28,29 @@ resource "aws_iam_role" "chat_server_s3_role" {
   name                 = "team1-dev-chat-server-s3-role"
   permissions_boundary = "arn:aws:iam::495599735720:policy/TeamRuntimeBoundary"
   assume_role_policy   = data.aws_iam_policy_document.chat_server_s3_trust.json
+
+  tags = {
+    Team        = "team1"
+    Environment = "dev"
+    Project     = "chatguard"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "chat_server_s3_readonly" {
-  role       = aws_iam_role.chat_server_s3_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+resource "aws_iam_role_policy" "chat_server_s3_readonly" {
+  name = "team1-dev-chat-server-s3-readonly"
+  role = aws_iam_role.chat_server_s3_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:GetObject", "s3:ListBucket"]
+      Resource = [
+        "arn:aws:s3:::team1-dev-chatguard-assets",
+        "arn:aws:s3:::team1-dev-chatguard-assets/*",
+        "arn:aws:s3:::team1-dev-chatguard-migration",
+        "arn:aws:s3:::team1-dev-chatguard-migration/*"
+      ]
+    }]
+  })
 }
