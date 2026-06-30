85dda9885e1a6c1b19f7c12ed72ab85658671b69
Shinsolmi <Shinsolmi>
2026-06-30 13:23:28 +0900

    refactor: 2차 PR 리뷰 피드백 [참고] 사항 반영

--- envs/dev/infra-base/backend.tf
+++ envs/dev/infra-base/backend.tf
@@ -1,6 +1,13 @@
 # envs/dev/infra-base/backend.tf
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
   backend "s3" {
     bucket       = "tfstate-lionkdt5-team1"
     key          = "team1/dev/infra-base/terraform.tfstate"
--- envs/dev/infra-base/budget.tf
+++ envs/dev/infra-base/budget.tf
@@ -1,7 +1,7 @@
 # envs/dev/infra-base/budget.tf
 
 resource "aws_sns_topic" "budget_alert_topic" {
-  name = "team1-dev-budget-alert-topic"
+  name = "team1-${var.env}-budget-alert-topic"
 }
 
 data "aws_iam_policy_document" "budget_chatbot_assume" {
@@ -15,13 +15,13 @@ data "aws_iam_policy_document" "budget_chatbot_assume" {
 }
 
 resource "aws_iam_role" "budget_chatbot_role" {
-  name                 = "team1-dev-budget-chatbot-role"
+  name                 = "team1-${var.env}-budget-chatbot-role"
   permissions_boundary = "arn:aws:iam::495599735720:policy/TeamRuntimeBoundary"
   assume_role_policy   = data.aws_iam_policy_document.budget_chatbot_assume.json
 }
 
 resource "aws_chatbot_slack_channel_configuration" "budget_slack_notifier" {
-  configuration_name = "team1-dev-budget-slack-chatbot"
+  configuration_name = "team1-${var.env}-budget-slack-chatbot"
   iam_role_arn       = aws_iam_role.budget_chatbot_role.arn
   slack_channel_id   = var.slack_channel_id
   slack_team_id      = var.slack_workspace_id
@@ -32,7 +32,7 @@ resource "aws_chatbot_slack_channel_configuration" "budget_slack_notifier" {
 }
 
 resource "aws_budgets_budget" "team_cost_budget" {
-  name              = "team1-dev-budget"
+  name              = "team1-${var.env}-budget"
   budget_type       = "COST"
   limit_amount      = "100"
   limit_unit        = "USD"
